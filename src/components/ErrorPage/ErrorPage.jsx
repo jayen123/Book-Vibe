1390e1088bfea82b908b250ccf956864c9dd598a
jayen123 <jayenhasan2003@gmail.com>
2024-11-04 00:14:15 +0600

    add error page

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ErrorPage() {
+  return (
+    <>
+      <h1 className="text-5xl">Page Not Found</h1>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    
   },
 ]);
 
